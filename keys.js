d742af24e8ea63a9d6de59acfcc6987946061874
伊藤友恒 <tomoito@itoutomohisashinoMacBook-Air.local>
2021-05-08 15:42:19 +0900

    api key のマスキング

--- keys.js
+++ keys.js
@@ -1,5 +1,5 @@
 //API KEY
-export const API_KEY = "AIzaSyCCjSiV3ktJe-F5aMAy6DMGuuV_0yliB_4";
+export const API_KEY = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
 
 //Context Key
-export const CONTEXT_KEY = "404d9e30ac98622a2";
+export const CONTEXT_KEY = "xxxxxxxxxxxxxxxxxxxxx";
